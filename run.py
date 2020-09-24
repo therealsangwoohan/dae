9c7ac1860f0b41b48e4c7dffb62ef75dea99bb5b
cyberpunkninja76 <cyberpunkninja76@gmail.com>
2020-09-24 18:00:58 -0400

    Updated.

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from dae import app
 
-if __name__ == '__main__':
-    print(6969696969696)
-    app.secret_key = "aSecretKey"
-    app.run(debug=True)
+app.secret_key = "aSecretKey"
+app.run(debug=True)
