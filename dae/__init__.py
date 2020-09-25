6e2b6a0cfa48f822d5521f22fb066736dd5a956d
cyberpunkninja76 <cyberpunkninja76@gmail.com>
2020-09-25 11:03:24 -0400

    Update.

--- dae/__init__.py
+++ dae/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = "aSecretKey"
 
 from dae.views import calendar, deadline_crud, event_crud
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from dae import app
 
 if __name__ == '__main__':
-    app.secret_key = "aSecretKey"
     app.run()
